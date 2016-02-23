dc5debf6b0f239770f0e5e5d276fbb44ec5a470e
root <root@zq.(none)>
2016-02-23 17:30:39 +0800

    check result of gethostbyname to fix Segmentation fault error

--- src/MQTTClient.c
+++ src/MQTTClient.c
@@ -1739,6 +1739,8 @@ int http_post_json(char *json_data, char *hostname, uint16_t port, char *path, C
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(port);
     struct hostent *host_entry = gethostbyname(hostname);
+	if(host_entry == NULL)
+		return -1;
     char* p = inet_ntoa(*((struct in_addr *)host_entry->h_addr));
 	if (inet_pton(AF_INET, p, &servaddr.sin_addr) <= 0)
 		return -1;
